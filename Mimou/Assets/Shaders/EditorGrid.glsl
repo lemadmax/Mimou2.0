9ba71818e24d4c298ae53e2e8191e2b6c3098c77
Zhengrui Xia <42914265+lemadmax@users.noreply.github.com>
2023-11-02 00:05:26 +0800

    grid start

---
+++ Mimou/Assets/Shaders/EditorGrid.glsl
@@ -0,0 +1,26 @@
+#type vertex
+#version 460 core
+
+uniform mat4 u_ViewProjection;
+uniform mat4 u_TransformMatrix;
+uniform mat4 u_InverseMatrix;
+
+vec3 gridPlane[6] = vec3[](
+    vec3(-1, 1, 0), vec3(1, 1, 0), vec3(-1, -1, 0), vec3(1, -1, 0)
+);
+
+void main()
+{
+    gl_Position = u_ViewProjection * vec4(gridPlane[gl_VertexID].xyz, 1.0);
+}	
+
+
+#type fragment
+#version 460 core
+
+layout(location = 0) out vec4 FragColor
+
+void main()
+{
+    FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
