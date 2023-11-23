a92f8b531dfeb756d30dfdb4ad43c1b0ecaf2988
Zhengrui Xia <42914265+lemadmax@users.noreply.github.com>
2023-11-23 23:24:24 +0800

    a little thing

--- Mimou/Source/Mimou/ECS/Entity/GameObject.h
+++ Mimou/Source/Mimou/ECS/Entity/GameObject.h
@@ -23,18 +23,18 @@ namespace Mimou
 		inline void SetParent(Ref<GameObject> Parent) { m_Parent = Parent; }
 		inline const entt::entity& GetEntityID() const { return m_EntityID; }
 
-		//template<typename T, typename... Args>
-		//T& AddComponent(Args&&... args)
-		//{
-		//	T& Component = m_Scene->m_Registry.emplace<T>(m_EntityID, std::forward<Args>(args)...);
-		//	return Component;
-		//}
+		template<typename T, typename... Args>
+		T& AddComponent(Args&&... args)
+		{
+			T& Component = m_Scene->m_Registry.emplace<T>(m_EntityID, std::forward<Args>(args)...);
+			return Component;
+		}
 
-		//template<typename T>
-		//T& GetComponent()
-		//{
-		//	return m_Scene->m_Registry.get<T>(m_EntityID);
-		//}
+		template<typename T>
+		T& GetComponent()
+		{
+			return m_Scene->m_Registry.get<T>(m_EntityID);
+		}
 
 	private:
 		entt::entity m_EntityID;
