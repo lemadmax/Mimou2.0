c5e892f13ee8ac170fdd3de3de6b57213b093f84
tonyxia <tonyxia@tencent.com>
2023-12-25 21:52:04 +0800

    open file continue

--- Mimou/PlatformUtils.h
+++ Mimou/PlatformUtils.h
@@ -5,7 +5,7 @@ namespace Mimou
 	class FileDialog
 	{
 	public:
-		static std::string OpenFile(const std::string& Filter);
+		static std::string OpenFile(const std::wstring& Filter);
 		static std::string SaveFile(const std::string& Filter);
 	};
 }
--- Mimou/WindowsPlatformUtils.cpp
+++ Mimou/WindowsPlatformUtils.cpp
@@ -10,17 +10,31 @@
 namespace Mimou
 {
 
-	std::string FileDialog::OpenFile(const std::string& Filter)
+	std::string FileDialog::OpenFile(const std::wstring& Filter)
 	{
 		OPENFILENAME ofn;
 		WCHAR szFile[260] = { 0 };
 		HWND hwnd = glfwGetWin32Window((GLFWwindow*)Application::GetInstance()->GetWindow().GetNativeWindow());
-		HANDLE fh;
+		HANDLE hf;
 
 		ZeroMemory(&ofn, sizeof(ofn));
 		ofn.lStructSize = sizeof(ofn);
 		ofn.hwndOwner = hwnd;
 		ofn.lpstrFile = szFile;
+		ofn.lpstrFile[0] = '\0';
+		ofn.nMaxFile = sizeof(szFile);
+		ofn.lpstrFilter = Filter.c_str();
+		ofn.nFilterIndex = 1;
+		ofn.lpstrFileTitle = (LPWSTR)L"Test Title";
+		ofn.nMaxFileTitle = 1;
+		ofn.lpstrInitialDir = NULL;
+		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
+
+		if (GetOpenFileName(&ofn) == TRUE)
+		{
+			std::wstring Out(ofn.lpstrFile);
+
+		}
 	}
 
 	std::string FileDialog::SaveFile(const std::string& Filter)
